13081a20fce2305ac76795c8293b256d73b3d6d8
Sakshi Sahu <115357999+Sakshi1s@users.noreply.github.com>
2024-01-05 22:53:31 +0530

    script.js

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
       link.addEventListener('click', smoothScroll);
     }
   
-    // Hover animation for social icons
     const socialIcons = document.querySelectorAll('.socials a');
     for (const icon of socialIcons) {
       icon.addEventListener('mouseover', addHoverAnimation);
       icon.addEventListener('mouseout', removeHoverAnimation);
     }
   
-    // Form submission handling
     const contactForm = document.forms['myform'];
     if (contactForm) {
       contactForm.addEventListener('submit', handleFormSubmission);
@@ -37,11 +35,5 @@ document.addEventListener('DOMContentLoaded', function () {
   
   function removeHoverAnimation() {
     this.classList.remove('hovered');
-  }
-  
-  function handleFormSubmission(e) {
-    e.preventDefault();
-    // You can add your form submission logic here
-    alert('Form submitted!'); // Placeholder, replace with your actual form submission code
   }
   
